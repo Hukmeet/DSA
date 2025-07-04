9d8b59e24bae0b5b40ff1e6c585af3d19054b7ec
Hukmeet Kaur Chhina <hukmeetkaur@gmail.com>
2025-07-04 11:45:53 +0530

    Day_1

---
+++ Learning Java/Bigger.java
@@ -0,0 +1,11 @@
+public class Bigger {
+    public static void main(String[] args) {
+        int a=5;
+        int b=7;
+        if (a>b)
+            System.out.print(a);
+        else 
+            System.out.print(b);
+        System.out.println(" is bigger!");
+    }
+}
